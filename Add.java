964bf7b3a4be6458670cdeb3a55d003ee16ca348
nagesh-13 <mekalanagesh31@gmail.com>
2026-08-11 12:22:21 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -1,6 +1,9 @@
+import java.util*;
 public class Add{
 public static void main(String args[]){
-int a=5,b=10;
+Scanner sc=new Scanner(system.in);
+  int a=sc.nextInt();
+  int b=sc.nextInt();
 System.out.println(a+b);
 }
 }
